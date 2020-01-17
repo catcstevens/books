ee0f7638c12112439ec46ad65970219729eb22ad
Catherine Callcott-Stevens <ccallcottstevens@gmail.com>
2020-01-17 19:01:45 +1100

    created books app

---
+++ app/controllers/books_controllers.rb
@@ -0,0 +1,32 @@
+class BooksController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    before_action :setup_data
+
+    def index
+        render json: @data
+    end
+
+    #Show a single book
+    def show
+    end
+
+    #Create a new book
+    def create
+    end
+
+    #Update a book
+    def update
+    end
+
+    #Remove a book
+    def destroy
+    end
+
+    private
+    def setup_data
+        @data = [
+            { title: "Harry Potter", author: "J.K Rowling" },
+            { title: "Name of the wind", author: "Patrick Rothfuss" }
+        ]
+    end
+end
